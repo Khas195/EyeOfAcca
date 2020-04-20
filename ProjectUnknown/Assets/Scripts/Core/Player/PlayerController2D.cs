3d38a615867a92b7e3852245d1d8b546c2d9efdf
Khas195 <tungthanhcao1091995@gmail.com>
2020-04-20 08:26:31 +0700

    Replace normal 2d movment behavior with 2d Movement for platform.

--- ProjectUnknown/Assets/Scripts/Core/Player/PlayerController2D.cs
+++ ProjectUnknown/Assets/Scripts/Core/Player/PlayerController2D.cs
@@ -8,24 +8,16 @@ public class PlayerController2D : MonoBehaviour
 {
     [SerializeField]
     Character2D character = null;
-    [SerializeField]
-    UnityEvent hackTrigger = new UnityEvent();
-    [SerializeField]
-    UnityEvent interactTrigger = new UnityEvent();
     // Update is called once per frame
     void Update()
     {
         var side = Input.GetAxisRaw("Horizontal");
         var forward = Input.GetAxisRaw("Vertical");
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            interactTrigger.Invoke();
-        }
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            hackTrigger.Invoke();
-        }
         character.Move(side, forward);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            character.Jump();
+        }
     }
 
     public void SetCharacter(Character2D targetCharacter)
