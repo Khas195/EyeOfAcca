85ff5a33359cc60410a3b71e4d950a34ece65830
Christopher <christopher.bukal@ue-germany.de>
2020-05-03 17:47:21 +0200

    animation test

---
+++ ProjectUnknown/Assets/Scripts/Chris_alt/ControlKeys.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlKeys : MonoBehaviour
+{
+
+    public static KeyCode KEY_JUMP = KeyCode.Space;
+
+}
