85022d36bc7eb73360ca3d9010c6947a41c8d6bb
Christopher <christopher.bukal@ue-germany.de>
2020-05-24 13:19:49 +0200

    Implemented cutting state and particles. lookin gooood

---
+++ ProjectUnknown/Assets/Scripts/Chris/FoliageStates.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoliageStates : MonoBehaviour
+{
+    [SerializeField]
+    private Animator myAnimator;
+
+    [SerializeField]
+    private ParticleSystem cutParticles;
+    [SerializeField]
+    private Transform particleSpawnpoint;
+
+    //Determine which vine sprite set to use (from three)
+    private void Awake()
+    {
+        this.myAnimator.SetInteger("EntryState", Random.Range(0,3));
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (Input.GetKeyDown(KeyCode.G))
+        {
+            this.TriggerCut();
+        }*/
+    }
+
+    //Triggers a sway state depending on the direction: less than zero trigger a left sway, triggers right sway otherwise
+    public void TriggerSway(int dir)
+    {
+        if (dir < 0)
+        {
+            this.myAnimator.SetTrigger("SwayL");
+        }
+        else
+        {
+            this.myAnimator.SetTrigger("SwayR");
+        }
+    }
+
+    //Triggers the cut state
+    public void TriggerCut()
+    {
+        this.myAnimator.SetTrigger("Cut");
+        Instantiate(this.cutParticles, this.particleSpawnpoint);
+    }
+}
---
+++ ProjectUnknown/Assets/Scripts/Chris/ParticleSystemKill.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSystemKill : MonoBehaviour
+{
+    private ParticleSystem myPS;
+
+    private void Awake()
+    {
+        this.myPS = this.GetComponent<ParticleSystem>();
+        if (!this.myPS)
+        {
+            Debug.LogError("No ParticleSystem found!");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!myPS.isPlaying)
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+}
