8ecdb1de7a60fbc0b921bdb82f2b8db12caf248f
Khas195 <tungthanhcao1091995@gmail.com>
2020-05-25 21:06:14 +0700

    added preview button to sfxs

--- ProjectUnknown/Assets/Scripts/Core/System/SFXResources.cs
+++ ProjectUnknown/Assets/Scripts/Core/System/SFXResources.cs
@@ -37,4 +37,42 @@ public class SFXResources : ScriptableObject
 
     [ReorderableList]
     public List<SFX> resourcesList = new List<SFX>();
+
+
+#if UNITY_EDITOR
+
+    [BoxGroup("Preview")]
+    [SerializeField]
+    bool reviewClip = false;
+    [BoxGroup("Preview")]
+    [SerializeField]
+    [HideIf("reviewClip")]
+    SFXList soundToPreview = SFXList.footStep_Run;
+    [BoxGroup("Preview")]
+    [SerializeField]
+    [ShowIf("reviewClip")]
+    AudioClip previewClip = null;
+    AudioSource previewer = null;
+    [Button]
+    public void Preview()
+    {
+        if (previewer == null)
+        {
+            previewer = UnityEditor.EditorUtility.CreateGameObjectWithHideFlags("Audio Preview", HideFlags.HideAndDontSave, typeof(AudioSource)).GetComponent<AudioSource>();
+        }
+
+        if (reviewClip == false)
+        {
+            var sound = resourcesList.Find(x => (x.tag.Equals(soundToPreview)));
+            previewer.volume = sound.volumn;
+            previewer.clip = sound.clip;
+        }
+        else
+        {
+            previewer.clip = previewClip;
+        }
+        previewer.Play();
+    }
+
+#endif
 }
