5dda0e6ef6789b0b20171f807069b98f8ab4371a
Ousama Andari <ousama.andari@ue-germany.de>
2020-06-13 22:20:04 +0200

    Editied the GameMasterSettings Script to unlock the mechanics

--- ProjectUnknown/Assets/Scripts/Core/System/GameMaster/GameMasterSettings.cs
+++ ProjectUnknown/Assets/Scripts/Core/System/GameMaster/GameMasterSettings.cs
@@ -33,8 +33,8 @@ public class GameMasterSettings : ScriptableObject
 
     public void Reset()
     {
-        LockGem();
-        LockRail();
+        UnlockGem();
+        UnlockRail();
     }
     public void UnlockGem()
     {
