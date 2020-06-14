44d0a57f829228bb08fd9f7401f5ced33d526ce4
Khas195 <tungthanhcao1091995@gmail.com>
2020-06-14 13:43:20 +0700

    Dynamic button in UI

---
+++ ProjectUnknown/Assets/Scripts/Core/Ultilities/ScaleTransition.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ScaleTransition : TransitionCurve
+{
+    [SerializeField]
+    Transform scaleTarget = null;
+    [SerializeField]
+    float highlightScale = 1.3f;
+    [SerializeField]
+    float normalScale = 1;
+    [SerializeField]
+    float pressScale = 1.2f;
+
+    float beginScale = 0;
+    float targetScale = 0;
+
+    protected override void Start()
+    {
+        base.Start();
+        beginScale = normalScale;
+        targetScale = normalScale;
+        SetScale(normalScale);
+    }
+    void Update()
+    {
+        if (this.IsCurrentTimeInGraph())
+        {
+            this.AdvanceTime(Time.deltaTime);
+            var value = this.GetCurrentValue();
+            SetScale(beginScale + (targetScale - beginScale) * value);
+        }
+
+    }
+
+    private void SetScale(float newScaleValue)
+    {
+        var newScale = scaleTarget.localScale;
+        newScale.x = newScale.y = newScale.z = newScaleValue;
+        scaleTarget.localScale = newScale;
+    }
+
+    public void UnHighlight()
+    {
+        beginScale = this.transform.localScale.x;
+        targetScale = normalScale;
+        this.TransitionIn();
+    }
+    public void Highlight()
+    {
+        beginScale = this.transform.localScale.x;
+        targetScale = highlightScale;
+        this.TransitionIn();
+    }
+    public void OnPress()
+    {
+        beginScale = this.transform.localScale.x;
+        targetScale = pressScale;
+        this.TransitionIn();
+    }
+    public void OnRelease()
+    {
+        beginScale = this.transform.localScale.x;
+        targetScale = normalScale;
+        this.TransitionIn();
+    }
+}
