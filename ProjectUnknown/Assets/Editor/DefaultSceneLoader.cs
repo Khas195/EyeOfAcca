b6a5d281eb287730bb90b178528527e42a8f0f3d
Khas195 <tungthanhcao1091995@gmail.com>
2020-06-27 17:57:31 +0700

    added default scene loader

---
+++ ProjectUnknown/Assets/Editor/DefaultSceneLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[InitializeOnLoadAttribute]
+public static class DefaultSceneLoader
+{
+    static DefaultSceneLoader()
+    {
+        EditorApplication.playModeStateChanged += LoadDefaultScene;
+    }
+
+    static void LoadDefaultScene(PlayModeStateChange state)
+    {
+        if (state == PlayModeStateChange.ExitingEditMode)
+        {
+            EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+        }
+
+        if (state == PlayModeStateChange.EnteredPlayMode)
+        {
+            if (EditorSceneManager.GetSceneByName("MasterScene").IsValid() == false)
+            {
+                GameSystemMenu.AddMasterScene();
+            }
+        }
+    }
+}
+#endif
