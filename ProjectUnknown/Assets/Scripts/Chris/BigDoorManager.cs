e6e0d8beb3608abdc500108692860655df068c49
Christopher <christopher.bukal@ue-germany.de>
2020-06-27 11:58:46 +0200

    Implemented indicator holder

---
+++ ProjectUnknown/Assets/Scripts/Chris/BigDoorManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigDoorManager : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer myRenderer;
+
+    [SerializeField]
+    private List<Sprite> stateSprites;
+
+    private int symbolStates;
+
+    private Animator myAnimator;
+
+    [SerializeField]
+    private List<ParticleSystem> myParticles;
+
+    private void Awake()
+    {
+        this.symbolStates = 0;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.myAnimator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            this.OpenDoor();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            this.UpdateSymbols();
+        }
+    }
+
+    public void UpdateSymbols()
+    {
+        this.myRenderer.sprite = this.stateSprites[++this.symbolStates];
+    }
+
+    public void OpenDoor()
+    {
+        this.myAnimator.SetTrigger("Open");
+        foreach (ParticleSystem ps in this.myParticles){
+            ps.Play();
+        }
+    }
+
+    public void StopParticles()
+    {
+        foreach (ParticleSystem ps in this.myParticles)
+        {
+            ps.Stop();
+        }
+    }
+
+
+}
