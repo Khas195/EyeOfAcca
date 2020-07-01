c69af3127221cecf68948b1dfed12ea5b5f4b35b
Khas195 <tungthanhcao1091995@gmail.com>
2020-07-01 18:18:32 +0700

    Improve door connections

---
+++ ProjectUnknown/Assets/Scripts/LevelConnection/LevelConnectionSettings.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[Serializable]
+public class DoorConnection
+{
+    [SerializeField]
+    [ReadOnly]
+    TransitionDoorProfile HomeDoor = null;
+
+    [SerializeField]
+    [Dropdown("AvailableDoors")]
+    TransitionDoorProfile LandingDoor = null;
+    public DoorConnection(ref TransitionDoorProfile home, ref TransitionDoorProfile land)
+    {
+        this.HomeDoor = home;
+        this.LandingDoor = land;
+    }
+    public void OnLandingDoorChanged()
+    {
+        HomeDoor.SetLandPlace(LandingDoor);
+    }
+    public DropdownList<TransitionDoorProfile> AvailableDoors()
+    {
+        var list = new DropdownList<TransitionDoorProfile>();
+        var profileList = Resources.LoadAll("GameSettings/LevelDoor", typeof(ScriptableObject));
+        foreach (var item in profileList)
+        {
+            var profile = (TransitionDoorProfile)item;
+            list.Add(profile.name, profile);
+        }
+        return list;
+    }
+}
+[Serializable]
+public class LevelDoorsSettings
+{
+    [OnValueChanged("OnSceneSet")]
+    [Scene]
+    public String sceneName;
+    public List<DoorConnection> doors = new List<DoorConnection>();
+    public void OnSceneSet()
+    {
+        var profileList = Resources.LoadAll("GameSettings/LevelDoor/" + sceneName, typeof(ScriptableObject));
+        doors.Clear();
+        foreach (var item in profileList)
+        {
+            var profile = (TransitionDoorProfile)item;
+            doors.Add(new DoorConnection(ref profile, ref profile.landingPlace));
+        }
+    }
+    public void OnDoorChanged()
+    {
+        for (int i = 0; i < doors.Count; i++)
+        {
+            doors[i].OnLandingDoorChanged();
+        }
+    }
+}
+[CreateAssetMenu(fileName = "LevelConnections", menuName = "Data/LevelConnection", order = 1)]
+public class LevelConnectionSettings : ScriptableObject
+{
+    [ReorderableList]
+    public List<LevelDoorsSettings> settings = new List<LevelDoorsSettings>();
+    [Button]
+    public void RefreshConnection()
+    {
+        for (int i = 0; i < settings.Count; i++)
+        {
+            settings[i].OnSceneSet();
+        }
+    }
+    [Button]
+    public void SAVE()
+    {
+        for (int i = 0; i < settings.Count; i++)
+        {
+            settings[i].OnDoorChanged();
+        }
+        RefreshConnection();
+    }
+}
